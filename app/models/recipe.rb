2293433ee4e3d2248e9681b6fa807cabcc7d6556
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-18 15:24:58 +0500

    Create recipe model make association and set validations

---
+++ app/models/recipe.rb
@@ -0,0 +1,14 @@
+class Recipe < ApplicationRecord
+    validates :name, presence: true
+    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :description, presence: true
+    
+    belongs_to :user
+    has_many :recipe_foods, dependent: :destroy
+    has_many :foods, through: :recipe_foods, dependent: :destroy
+
+    def total_price
+        foods.sum('price * recipe_foods.quantity')
+    end
+end
