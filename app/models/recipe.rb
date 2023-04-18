3052ea134f7a371cfcf17982083f177f8d7eeab9
blase147 <solarmails2@gmail.com>
2023-04-18 17:56:10 +0100

    Add public recipe list

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+  #   has_many :recipe_foods, dependent: :destroy
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :cooking_time, presence: true
+  validates :preparation_time, presence: true
+  validates :description, presence: true
+end
