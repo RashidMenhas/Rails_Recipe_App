62a18f4ea8dc2423787c29646e15f6c32556e237
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-18 15:34:22 +0500

    Fix linter errors.

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,6 +2,6 @@ class RecipeFood < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :food, uniqueness: { scope: :recipe, case_sensitive: false, message: 'already exists in this recipe' }
 
-    belongs_to :recipe
-    belongs_to :food
+  belongs_to :recipe
+  belongs_to :food
 end
