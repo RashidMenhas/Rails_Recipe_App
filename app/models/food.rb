561b326c98936885630b9a2b1da676ad349ca867
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-18 15:33:28 +0500

    Fix indentaion.

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,7 +4,6 @@ class Food < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
-    belongs_to :user
-    has_many :recipe_foods
+  belongs_to :user
+  has_many :recipe_foods
 end
