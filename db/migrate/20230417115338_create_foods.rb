33b542c36d11b102c2c2102d5983af8122c837a8
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-18 15:25:57 +0500

    Create migration for user, food, recipe, recipe_food

---
+++ db/migrate/20230417115338_create_foods.rb
@@ -0,0 +1,14 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit
+      t.decimal :price, precision:5, scale: 2
+      t.integer :quantity
+      t.references :user, null: false, foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end
