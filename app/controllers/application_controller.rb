4ddbdb60cc55c48f5f8f22f49792c101439a6f98
Rashid Menhas <rashidmenhas10@gmail.com>
2023-04-19 19:56:57 +0500

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-    protect_form_forgery with: :exceptions
+  protect_form_forgery with: :exceptions
 
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def after_sign_in_path_for(_resource)
-        #users_url
+  def after_sign_in_path_for(_resource)
+    # users_url
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
     end
-
-    protected
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-        devise_parameter_sanitizer.permit(:account_update) do |u|
-          u.permit(:name, :email, :password, :password_confirmation, :current_password)
-        end
-    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, presence: true
 
   has_many :recipes, dependent: :destroy
